16209bafd1621b0df6e320c1bd09d6818adb2435
Winn4K <kauan99436601@gmail.com>
2024-08-19 15:57:40 -0300

    Ajuste da logo do Header, e adicionando links dinamicos aos projetos

---
+++ assets/js/MOCK.js
@@ -0,0 +1,83 @@
+export const MOCK_PROJETOS = [
+    {
+        id: 1,
+        nome: 'E-Commerce de Roupas',
+        descricao: 'Desenvolvi um e-commerce de roupas para testar minhas habilidades com React e React Router para paginação de componentes e telas.',
+        tecnologias: 'React - JavaScript - SASS',
+        imagem: '../assets/imgs/projetos/e-commerce.png',
+        linkGithub: 'https://github.com/Winn4K/E-commerce-para-loja-de-roupas',
+        linkProjeto: 'https://mini-project-ecommerce.netlify.app/'
+    },
+    {
+        id: 2,
+        nome: 'Landing Page de Escola Online',
+        descricao: 'Desenvolvi esta langing page que apresenta uma escola online, mostrando todos os seus beneficios, ao final da página, criei um formulário para captação de clientes.',
+        tecnologias: 'HTML - CSS - JavaScript',
+        imagem: '../assets/imgs/projetos/LandingPage-Escola-Online.png',
+        linkGithub: 'https://github.com/Winn4K/Landing-Page-Escola-Online',
+        linkProjeto: 'https://landingpage-teste-dnc.netlify.app/'
+    },
+    {
+        id: 3,
+        nome: 'Landing Page Empresa de Arquitetura',
+        descricao: 'Desenvolvi esta langing page que apresenta uma rede de arquitetos associados, mostrando todos os seus beneficios, ao final da página, criei um formulário para captação de clientes.',
+        tecnologias: 'HTML - CSS - JavaScript',
+        imagem: '../assets/imgs/projetos/LandingPage-Arquitetura.png',
+        linkGithub: 'https://github.com/Winn4K/Landing-Page-Empresa-de-Arquitetura',
+        linkProjeto: 'https://desafio-landingpage-arquitetos.netlify.app/'
+    },
+    {
+        id: 4,
+        nome: 'Meu Portifólio',
+        descricao: 'Desenvolvi meu portifólio para me apresentar, apresentar meus projetos, as tecnologias que domino e falar um pouco de mim.',
+        tecnologias: 'HTML - CSS - JavaScript',
+        imagem: '../assets/imgs/projetos/Portifolio.jpeg',
+        linkGithub: 'https://github.com/Winn4K/Portifolio',
+        linkProjeto: 'https://kauan-rodrigues.netlify.app/'
+    }
+]
+
+export const MOCK_TECNOLOGIAS = [
+    {
+        id: 1,
+        nome: 'Javascript',
+        descricao: 'O JavaScript é uma linguagem de programação de alto nível voltada para o desenvolvimento web. Originalmente criado para funcionar nos navegadores, ele permite criar páginas interativas com elementos dinâmicos e boa performance',
+        classeIcone: 'fa-brands fa-js',
+    },
+    {
+        id: 2,
+        nome: 'HTML',
+        descricao: 'O HTML5 é a evolução mais recente da linguagem de marcação de hipertexto, permitindo a criação de sites modernos e compatíveis com qualquer navegador. Ele expande o HTML clássico com novas tags e recursos',
+        classeIcone: 'fa-brands fa-html5',
+    },
+    {
+        id: 3,
+        nome: 'CSS',
+        descricao: 'CSS é uma linguagem de estilo usada para controlar a apresentação visual de documentos HTML. Enquanto o HTML define a estrutura e o conteúdo de uma página web, o CSS determina como esse conteúdo é estilizado e exibido aos usuários',
+        classeIcone: 'fa-brands fa-css3',
+    },
+    {
+        id: 4,
+        nome: 'React',
+        descricao: 'O React é uma biblioteca JavaScript usada para criar interfaces de usuário (UI) em aplicativos web. Ele permite dividir uma página em partes chamadas de componentes, facilitando o desenvolvimento e a manutenção do código',
+        classeIcone: 'fa-brands fa-react',
+    },
+    {
+        id: 5,
+        nome: 'Bootstrap',
+        descricao: 'Bootstrap é um framework front-end de código aberto que combina CSS e JavaScript para estilizar elementos de páginas escritas em HTML. Ele fornece componentes prontos para agilizar o desenvolvimento web, permitindo criar sites e aplicações responsivas',
+        classeIcone: 'fa-brands fa-bootstrap',
+    },
+    {
+        id: 6,
+        nome: 'SASS',
+        descricao: 'SASS (Syntactically Awesome Style Sheets) é um pré-processador de CSS que permite escrever estilos com uma sintaxe mais intuitiva e adicionar recursos não presentes no CSS puro',
+        classeIcone: 'fa-brands fa-sass',
+    },
+    {
+        id: 7,
+        nome: 'Java',
+        descricao: 'Java é uma linguagem de programação orientada a objetos que permite os desenvolvedores escreverem programas uma vez e executa-los em diferentes dispositivos, sendo amplamente usada no desenvolvimento de aplicativos e serviços digitais',
+        classeIcone: 'fa-brands fa-java',
+    }
+]
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,114 @@
-window.addEventListener('scroll', function() {
+import { MOCK_PROJETOS } from './MOCK.js';
+import { MOCK_TECNOLOGIAS } from './MOCK.js';
+
+function mapearProjetos() {
+
+    const dados = MOCK_PROJETOS;
+    const projetos = document.getElementById('projetos');
+
+    for (let i = 0; i < dados.length; i++) {
+
+        const novoProjeto = document.createElement('div');
+        novoProjeto.className += 'projeto ';
+        novoProjeto.className += 'change-color-primary ';
+        novoProjeto.className += 'zoom';
+
+        const popupProjeto = document.createElement('div');
+        popupProjeto.className += 'linksProjetos'
+
+        const linkGithub = document.createElement('a');
+        linkGithub.href = dados[i].linkGithub;
+
+        popupProjeto.appendChild(linkGithub);
+
+        const iconeGithub = document.createElement('i');
+        iconeGithub.className += 'fa-brands fa-github-alt iconesProjetos';
+
+        linkGithub.appendChild(iconeGithub);
+
+        const linkProjeto = document.createElement('a');
+        linkProjeto.href = dados[i].linkProjeto;
+
+        popupProjeto.appendChild(linkProjeto);
+
+        const iconeSite = document.createElement('i');
+        iconeSite.className += 'fa-solid fa-laptop-code iconesProjetos';
+
+        linkProjeto.appendChild(iconeSite);
+
+        novoProjeto.appendChild(popupProjeto);
+
+        const imagem = document.createElement('img');
+        imagem.src = dados[i].imagem;
+        imagem.alt = dados[i].nome;
+        novoProjeto.appendChild(imagem);
+
+        const projetoText = document.createElement('div');
+        projetoText.className += 'projeto-text';
+
+        novoProjeto.appendChild(projetoText);
+
+        const titulo = document.createElement('h5');
+        titulo.innerText = dados[i].nome;
+        const conteudo = document.createElement('p');
+        conteudo.innerText = dados[i].descricao;
+        const tecnologias = document.createElement('p');
+        tecnologias.innerText = dados[i].tecnologias;
+
+        projetoText.appendChild(titulo);
+        projetoText.appendChild(conteudo);
+        projetoText.appendChild(tecnologias);
+
+        projetos.appendChild(novoProjeto);
+
+        novoProjeto.addEventListener('mouseenter', () => {
+            popupProjeto.style.display = 'flex';
+        });
+
+        novoProjeto.addEventListener('mouseleave', () => {
+            popupProjeto.style.display = 'none';
+        });
+    };
+
+};
+
+function mapearTecnologias() {
+    const dados = MOCK_TECNOLOGIAS;
+    const tecnologias = document.getElementById('tecnologias');
+
+    for (let i = 0; i < dados.length; i++) {
+
+        const cardTecnologia = document.createElement('div');
+        cardTecnologia.className += 'card-tecnologia ';
+        cardTecnologia.className += 'change-color-secondary ';
+        cardTecnologia.className += ' zoom';
+
+        const icone = document.createElement('i');
+        icone.className += dados[i].classeIcone;
+
+        cardTecnologia.appendChild(icone);
+
+        const titulo = document.createElement('h5');
+        titulo.innerText += dados[i].nome;
+
+        cardTecnologia.appendChild(titulo);
+
+        const descricao = document.createElement('p');
+        descricao.innerText += dados[i].descricao;
+
+        cardTecnologia.appendChild(descricao);
+
+        tecnologias.appendChild(cardTecnologia);
+
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    mapearProjetos();
+    mapearTecnologias();
+});
+
+window.addEventListener('scroll', function () {
     const header = document.querySelector('header');
     if (window.scrollY > 1) {
         header.classList.add('fixed-header');
